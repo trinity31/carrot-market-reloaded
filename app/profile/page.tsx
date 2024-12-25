0c1cc4c6cb3aaa15ca55d341f5b48ebb11447fea
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2024-12-25 12:07:14 +0900

    프로필 페이지, 로그아웃

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,33 @@
-export default function Profile() {
-    return <div>Welcome to your Profile</div>;
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
+
+
+async function getUser() {
+    const session = await getSession();
+    if (session.id) {
+        const user = await db.user.findUnique({
+            where: {
+                id: session.id,
+            },
+        });
+        return user;
+    }
+    notFound(); //session id 가 없거나 user 가 없는 경우
+}
+
+export default async function Profile() {
+    const user = await getUser();
+    const logOut = async () => {
+        "use server";
+        const session = await getSession();
+        await session.destroy();
+        redirect("/");
+    }
+    return (
+        <div>
+            <h1>Welcome {user?.username}</h1>
+            <form action={logOut}> <button>Log out</button></form>
+        </div>
+    );
 }
