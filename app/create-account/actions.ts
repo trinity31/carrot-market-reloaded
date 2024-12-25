48b2999008f7382c2632a9bc1ba9521c925bcf69
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2024-12-25 11:48:15 +0900

    Create Account - superRefine 으로 리팩토링

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,34 +4,21 @@ import { z } from "zod";
 import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
 import getSession from "@/lib/session";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
 
 const passwordRegex = new RegExp(
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
 );
 
-const checkUsernameIsUnique = async (username: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      username,
-    },
-    select: {
-      id: true,
-    },
-  });
-  return !Boolean(user);
-}
+const checkUsername = (username: string) => !username.includes("potato");
 
-const checkEmailIsUnique = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      email,
-    },
-    select: {
-      id: true,
-    },
-  });
-  return !Boolean(user);
-}
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
 
 const formSchema = z
   .object({
@@ -40,34 +27,56 @@ const formSchema = z
         invalid_type_error: "Username must be a string!",
         required_error: "Where is my username???",
       })
-      .min(3, "Way too short!!!")
-      //.max(10, "That is too looooong!")
-      .trim()
       .toLowerCase()
-     // .transform((username) => `🔥 ${username}`)
-      .refine(
-        (username) => !username.includes("potato"),
-        "No potatoes allowed!"
-      ).refine(checkUsernameIsUnique, "username is already taken"),
-    email: z.string().email().toLowerCase()
-      .refine(checkEmailIsUnique, "email is already taken"),
-    password: z
-      .string()
-      .min(4),
-      // .regex(
-      //   passwordRegex,
-      //   "Passwords must contain at least one UPPERCASE, lowercase, number and special characters #?!@$%^&*-"
-      // ),
-    confirm_password: z.string().min(4),
+      .trim()
+      // .transform((username) => `🔥 ${username} 🔥`)
+      .refine(checkUsername, "No potatoes allowed!"),
+    email: z.string().email().toLowerCase(),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    //.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
-  .superRefine(({ password, confirm_password }, ctx) => {
-    if (password !== confirm_password) {
+  .superRefine(async ({ username }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
       ctx.addIssue({
         code: "custom",
-        message: "Two passwords should be equal",
-        path: ["confirm_password"],
+        message: "This username is already taken",
+        path: ["username"],
+        fatal: true,
       });
+      return z.NEVER;
     }
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: "custom",
+        message: "This email is already taken",
+        path: ["email"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
